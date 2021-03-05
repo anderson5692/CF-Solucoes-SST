97aa518eee95f532bdaa579a09cdf94fc50e4744
anderson5692 <blueoystercult1982@gmail.com>
2021-03-04 22:11:44 -0300

    start app

---
+++ cfvenv/Scripts/CF_APP/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CfAppConfig(AppConfig):
+    name = 'CF_APP'
---
+++ cfvenv/Scripts/CF_APP/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    #d1 = {'':""}
+    return render(request, 'django/index.html')
+    return render(request, 'django/page1.html')
