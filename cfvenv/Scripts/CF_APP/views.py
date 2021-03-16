e12a89d82a3cf1fdc8292ed09b36b51b14c71c6b
anderson5692 <blueoystercult1982@gmail.com>
2021-03-15 22:13:46 -0300

    urls adjust

--- cfvenv/Scripts/CF_APP/views.py
+++ cfvenv/Scripts/CF_APP/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
+def index(request):
     return render(request, 'django/index.html')
-    return render(request, 'django/page1.html')
+    
--- cfvenv/Scripts/django_project/urls.py
+++ cfvenv/Scripts/django_project/urls.py
@@ -13,15 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from CF_APP import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home, name='home')
+    path('index/', include('')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
